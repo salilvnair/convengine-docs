58f81cc456d29105281ded54f5a9561c09a51750
salilvnair <salilvnairgithub@gmail.com>
2026-01-29 00:17:42 -0600

    gh changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,17 +17,17 @@ const config = {
   future: {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
-
+  trailingSlash: false,
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://salilvnair.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/convengine-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'salilvnair', // Usually your GitHub org/user name.
-  projectName: 'convengine', // Usually your repo name.
+  projectName: 'convengine-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
