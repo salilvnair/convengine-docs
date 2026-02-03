d38d564f9dad53c32a9ac7056f5a02b6ea4da299
salilvnair <salilvnairgithub@gmail.com>
2026-02-03 00:54:31 -0600

    Update base URL in Docusaurus config for proper site deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
   url: 'https://salilvnair.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/convengine-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
